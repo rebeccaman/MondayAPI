592a49d73c55194b7a26bdf6c2f65dc611b51702
Rebecca Manhem <rebecca.manhem@gmail.com>
2021-07-05 10:56:55 +0200

    Removed ignore files

---
+++ src/main/java/com/MondayAPI/MondayApiApplication.java
@@ -0,0 +1,13 @@
+package com.MondayAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MondayAPI/MondayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MondayAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MondayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
